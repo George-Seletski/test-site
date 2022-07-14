abf272d9e539bad483a9b10402e32ca5735f28ad
[George Seletski] <[george.seletski.1224@gmail.com]>
2022-07-14 19:55:22 +0300

    feat: designed the html template

--- website/news/admin.py
+++ website/news/admin.py
@@ -1,12 +1,18 @@
-from turtle import title
 from django.contrib import admin
-from .models import News
-
+from .models import News,Category
 
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'created_at', 'updated_at', 'is_published')
+    list_display = ('id', 'title', 'category', 'created_at', 'updated_at', 'is_published')
     list_display_links = ('id', 'title')  # links to models
     search_fields = ('title', 'content')  # filds for searching
 
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    list_display_links = ('id', 'title')  # links to models
+    search_fields = ('title',)  # filds for searching
+
+
 admin.site.register(News, NewsAdmin)
+admin.site.register(Category, CategoryAdmin)
+
--- website/news/models.py
+++ website/news/models.py
@@ -1,6 +1,4 @@
-from calendar import c
-from operator import mod
-from tabnanny import verbose
+
 from django.db import models
 
 # Create your models here.
@@ -13,6 +11,7 @@ class News(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     is_published = models.BooleanField(default=True)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
 
     #id -INT
     #title -VARCHAR
@@ -29,3 +28,15 @@ class News(models.Model):
         verbose_name = 'News'
         verbose_name_plural = 'News'
         ordering = ['-created_at']
+    
+class Category(models.Model):
+    title = models.CharField(max_length=150, db_index=True, verbose_name="Name of category")
+
+    def __str__(self):
+        return self.title
+    
+        
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Category'
+        ordering = ['title']
